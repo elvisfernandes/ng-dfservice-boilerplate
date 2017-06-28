c3eb50421ed1e65fb99d70bd78b1e3681143ccdf
Elvis Fernandes <elvis@bizconsult.com.br>
2017-06-28 13:00:24 -0300

    Adding proper providers, as per README

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+
+import { DFService } from 'ng-dfservice';
+import { MY_DATASTORE_PROVIDERS } from './dfdatastores/myDataStores';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    MY_DATASTORE_PROVIDERS,
+    { provide: DFService.DFSERVICE_CONFIG, useValue: environment.dfservice_config }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
